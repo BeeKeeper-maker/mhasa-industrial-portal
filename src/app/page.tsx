312c69aa46133d9721588c1f8c4f804690803074
Faysal Hamed <faysalhamed1998@gmail.com>
2026-07-22 10:47:58 +0600

    Fix layout architecture: move Header, Footer and global overlays into RootLayout to ensure they appear on all routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-import { Header } from "@/components/site/header";
-import { Footer } from "@/components/site/footer";
-import { BackToTop, FloatingActions, CookieConsent } from "@/components/site/floating-ui";
-import { SearchDialog } from "@/components/site/search-dialog";
-import { QuickQuoteWidget } from "@/components/site/quick-quote-widget";
-import { AdminOverlay } from "@/components/admin/admin-overlay";
-import { RtlRoot } from "@/components/site/rtl-root";
 import { HomeView } from "@/components/views/home-view";
 
 export default function Home() {
-  return (
-    <RtlRoot>
-      <div className="flex min-h-screen flex-col bg-background">
-        <Header />
-        <main className="flex-1">
-          <HomeView />
-        </main>
-        <Footer />
-        <BackToTop />
-        <FloatingActions />
-        <CookieConsent />
-        <SearchDialog />
-        <QuickQuoteWidget />
-        <AdminOverlay />
-      </div>
-    </RtlRoot>
-  );
+  return <HomeView />;
 }
